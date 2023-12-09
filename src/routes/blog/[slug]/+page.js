fdc5c9e7ebdf88786cef01a172c5b4fc981e268e
notunrandom <notunrandom@proton.me>
2023-12-09 13:02:45 +0100

    Alternative method (refactoring).

--- src/routes/blog/[slug]/+page.js
+++ src/routes/blog/[slug]/+page.js
@@ -1,6 +1,6 @@
 export async function load({params}) {
     const post = await import(`../${params.slug}.md`);
     const {title, date} = post.metadata;
-    const content = post.default;
-    return {content, title, date};
+    const Content = post.default;
+    return {title, date, Content};
 }
